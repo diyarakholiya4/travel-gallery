f54a0b2a53da013301fecd484fca6c74b9bd3a3b
diyarakholiya4 <diyarakholiya4@gmail.com>
2026-03-31 21:27:19 +0530

    Delete website directory

--- website/navbar.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("navbar").innerHTML = `
-  <nav>
-    <a href="index.html">Home</a>
-    <a href="bhutan.html">Bhutan</a>
-    <a href="maldives.html">Maldives</a>
-  </nav>
-`;
