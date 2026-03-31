a689a3e45ac2f28818f23b07bb759323cc6c28cf
diyarakholiya4 <diyarakholiya4@gmail.com>
2026-03-31 21:04:35 +0530

    Create navbar.js

---
+++ website/navbar.js
@@ -0,0 +1,7 @@
+document.getElementById("navbar").innerHTML = `
+  <nav>
+    <a href="index.html">Home</a>
+    <a href="bhutan.html">Bhutan</a>
+    <a href="maldives.html">Maldives</a>
+  </nav>
+`;
